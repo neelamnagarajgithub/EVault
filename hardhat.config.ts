d33cf6630baff23924731da623c25d1f3b7cc103
Neelam Naga Raj <113004986+neelamnagarajgithub@users.noreply.github.com>
2024-05-30 11:40:52 +0530

    Configuring Networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {SEPOLIA_URL,SECRET_KEY}=process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+    networks: {
+       sepolia: {
+         url: SEPOLIA_URL,
+         accounts: SECRET_KEY!==undefined?[SECRET_KEY]:[] ,
+       },
+    },
 };
 
 export default config;
