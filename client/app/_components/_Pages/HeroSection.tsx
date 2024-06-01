e96b55e17e7012bcadb1b7dffed43ae91c41503e
Neelam Naga Raj <113004986+neelamnagarajgithub@users.noreply.github.com>
2024-06-01 17:58:30 +0530

    HeroSection Update

--- client/app/_components/_Pages/HeroSection.tsx
+++ client/app/_components/_Pages/HeroSection.tsx
@@ -12,7 +12,25 @@ export default function HeroSection() {
         />
       </div>
       <div className=" h-screen w-full flex justify-center items-center px-40">
-        <h1 className=" text-white">hello world</h1>
+        <div className="flex gap-2 flex-col">
+          <h1 className=" text-white text-5xl font-mono font-bold">
+            Revolutionizing Legal Record Management with Blockchain Technology
+          </h1>
+          <p className=" text-white">
+            Join us in creating a secure, transparent, and efficient eVault
+            system for legal records.
+          </p>
+          <Link
+            href="/account"
+            className=" bg-blue-800 font-semibold font-mono text-2xl w-max px-4 py-2 rounded-lg text-white hover:bg-blue-900 transition-all duration-300 ease-in-out mt-4 flex justify-center items-center gap-1 hover:gap-2"
+          >
+            Get started
+            <span>
+              <BiArrowFromLeft />
+            </span>
+          </Link>
+        </div>
+        {/* <Scene /> */}
       </div>
     </section>
   );
