cf5ca93d395b4120adc361a27386df5d12af744a
Rithwik3425 <rithwikchimmani3425@gmail.com>
2024-06-02 00:52:31 +0530

    changes in the footer

--- client/app/_components/_Pages/Footer.tsx
+++ client/app/_components/_Pages/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="w-full bg-blue-800 h-auto px-40 flex  py-10 flex-col justify-center items-center gap-8">
       <div className="w-full flex justify-between items-center">
-        <div className="flex flex-col justify-center items-center gap-8">
+        <div className="flex justify-center items-center gap-8">
           <Link href="/about" className="text-white text-2xl font-mono">
             About
           </Link>
@@ -17,7 +17,7 @@ export default function Footer() {
         </div>
         <h1 className="text-white text-4xl font-mono">EVault</h1>
       </div>
-      <div className="text-white">copyrights EVault&copy; </div>
+      <div className="text-white text-xl">copyrights EVault&copy; </div>
     </footer>
   );
 }
