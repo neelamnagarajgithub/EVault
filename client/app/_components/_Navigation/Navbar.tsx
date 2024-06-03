7edc2ebd8ffacae04384e6bbbd2de22b233401cd
Rithwik3425 <rithwikchimmani3425@gmail.com>
2024-06-04 02:25:44 +0530

    minor changes in navbar

--- client/app/_components/_Navigation/Navbar.tsx
+++ client/app/_components/_Navigation/Navbar.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
 import NavElements from "./NavElements";
 
 export default function Navbar() {
   // const isopen = useSelector((state: RootState) => state.user.showMenu);
   return (
     <div className="flex fixed w-full h-[10vh] backdrop-blur md:justify-between justify-center items-center text-center z-[100] left-0 top-0 flex-wrap px-40">
-      <h1 className=" text-3xl text-white ml-2 font-mono font-semibold">
+      <Link
+        className=" text-3xl text-white ml-2 font-mono font-semibold"
+        href="/"
+      >
         EVault
-      </h1>
+      </Link>
       <NavElements />
       {/* <NavMobileElements /> */}
     </div>
